02196ebd1998e60de99b322997b6deac61ca9037
Kaj-Michael Lang <milang@tal.org>
2025-03-09 10:37:56 +0200

    Implement animation helper

--- src/animationcurve.cpp
+++ src/animationcurve.cpp
@@ -1,12 +1,65 @@
 #include "animationcurve.h"
 
 AnimationCurve::AnimationCurve(QObject *parent)
-    : QObject{parent}
+    : QObject{parent},
+    m_from(0),
+    m_to(1)
 {
-    setType(QEasingCurve::InOutQuad);
+    QEasingCurve::setType(QEasingCurve::InOutQuad);
 }
 
 qreal AnimationCurve::valueAt(qreal position)
 {
+    qreal dif=m_to-m_from;
+
+    return m_from+valueForProgress(position)*dif;
+}
+
+qreal AnimationCurve::valueAtNormalized(qreal position)
+{
     return valueForProgress(position);
+}
+
+void AnimationCurve::setAmplitude(qreal amplitude)
+{
+    QEasingCurve::setAmplitude(amplitude);
+}
+
+qreal AnimationCurve::from() const
+{
+    return m_from;
+}
+
+void AnimationCurve::setFrom(qreal newFrom)
+{
+    if (qFuzzyCompare(m_from, newFrom))
+        return;
+    m_from = newFrom;
+    emit fromChanged();
+}
+
+qreal AnimationCurve::to() const
+{
+    return m_to;
+}
+
+void AnimationCurve::setTo(qreal newTo)
+{
+    if (qFuzzyCompare(m_to, newTo))
+        return;
+    m_to = newTo;
+    emit toChanged();
+}
+
+QEasingCurve::Type AnimationCurve::type() const
+{
+    return QEasingCurve::type();
+}
+
+void AnimationCurve::setType(const QEasingCurve::Type newType)
+{
+    if (type() == newType)
+        return;
+    QEasingCurve::setType(newType);
+    emit typeChanged();
 }
--- src/animationcurve.h
+++ src/animationcurve.h
@@ -11,9 +11,34 @@ class AnimationCurve : public QObject, QEasingCurve
 public:
     explicit AnimationCurve(QObject *parent = nullptr);
 
+    Q_PROPERTY(qreal from READ from WRITE setFrom NOTIFY fromChanged FINAL)
+    Q_PROPERTY(qreal to READ to WRITE setTo NOTIFY toChanged FINAL)
+
+    Q_PROPERTY(QEasingCurve::Type type READ type WRITE setType NOTIFY typeChanged FINAL)
+
     Q_INVOKABLE qreal valueAt(qreal position);
+    Q_INVOKABLE qreal valueAtNormalized(qreal position);
+
+    Q_INVOKABLE void setType(QEasingCurve::Type type);
+    Q_INVOKABLE void setAmplitude(qreal amplitude);
+
+    qreal from() const;
+    Q_INVOKABLE void setFrom(qreal newFrom);
+
+    qreal to() const;
+    Q_INVOKABLE void setTo(qreal newTo);
+
+    QEasingCurve::Type type() const;
 
 signals:
+
+    void fromChanged();
+    void toChanged();
+    void typeChanged();
+
+private:
+    qreal m_from;
+    qreal m_to;
 };
 
 #endif // ANIMATIONCURVE_H
